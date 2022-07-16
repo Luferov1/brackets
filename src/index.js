e6be876e2c9a1c6d3e2d84e8f21bb8c78be9727b
Luferov1 <ldima12022002@gmail.com>
2022-07-16 18:33:03 +0300

    feat: find part of solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+  const newConfig = bracketsConfig.reduce( (prev, item) => ([...prev, ...item]), []);
+  for (let item of newConfig) {
+    if (!str.includes(item)) return false;
+    const arr = str.split('').filter( (item) => (item !== '|' && item !== '7' && item !== '8'));
+    let sum = 0;
+    for (let item of arr) {
+      if (newConfig.indexOf(item) % 2 === 0) sum++;
+      else sum--;
+      if (sum < 0) return false;
+    }
+    return true;
+  }
 }
