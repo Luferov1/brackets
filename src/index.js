58f5b4f64268ce6530c625c308aea919a5a355ed
Luferov1 <ldima12022002@gmail.com>
2022-07-25 15:58:16 +0300

    feat: add final solution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,61 @@
 module.exports = function check(str, bracketsConfig) {
   if (str.length % 2 !== 0) return false;
   const newConfig = bracketsConfig.reduce( (prev, item) => ([...prev, ...item]), []);
+  console.log(newConfig);
+  let bracketsToClose = [];
+  let specialBrackets1 = 0;
+  let specialBrackets2 = 0;
+  let specialBrackets3 = 0;
   for (let item of newConfig) {
     if (!str.includes(item)) return false;
-    const arr = str.split('').filter( (item) => (item !== '|' && item !== '7' && item !== '8'));
-    let sum = 0;
+    const arr = str.split('');
     for (let item of arr) {
-      if (newConfig.indexOf(item) % 2 === 0) sum++;
-      else sum--;
-      if (sum < 0) return false;
+      let i = newConfig.indexOf(item);
+      if (item === '|' || item === '7' || item === '8') {
+        if (item === '|') {
+          if (specialBrackets1 === 0) {
+            bracketsToClose.push(item);
+            specialBrackets1++;
+          }
+          else {
+            if (item !== bracketsToClose[bracketsToClose.length - 1]) return false;
+            specialBrackets1--;
+            bracketsToClose.pop();
+          }
+        }
+        else if (item === '7') {
+          if (specialBrackets2 === 0) {
+            bracketsToClose.push(item);
+            specialBrackets2++;
+          }
+          else {
+            if (item !== bracketsToClose[bracketsToClose.length - 1]) return false;
+            specialBrackets2--;
+            bracketsToClose.pop();
+          }
+        }
+        else {
+          if (specialBrackets3 === 0) {
+            bracketsToClose.push(item);
+            specialBrackets3++;
+          }
+          else {
+            if (item !== bracketsToClose[bracketsToClose.length - 1]) return false;
+            specialBrackets3--;
+            bracketsToClose.pop();
+          }
+        }
+        
+      }
+
+      else if (i % 2 === 0) {
+        bracketsToClose.push(newConfig[i+1]);
+      }
+
+      else {
+        if (item !== bracketsToClose[bracketsToClose.length - 1]) return false;
+        bracketsToClose.pop();
+      }
     }
     return true;
   }
